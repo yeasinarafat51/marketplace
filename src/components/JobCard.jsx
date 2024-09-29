5e7081cc1ddf9a6d92cbd059aa73ac5412636226
yeasinarafat51 <aloondokar65@gmail.com>
2024-09-29 21:53:50 +0600

    card

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -3,7 +3,15 @@
 import { Link } from "react-router-dom"
 
 const JobCard = ({job}) => {
-    const {_id, job_title, category}= job || {}
+    const {
+        _id,
+         job_title, 
+        description,
+        min_price,
+        max_price,
+        category,
+        deadline,
+        bid_count,}= job || {}
     return (
       <Link to={`/job/${_id}`} className='w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all'>
         <div className='flex items-center justify-between'>
@@ -20,11 +28,11 @@ const JobCard = ({job}) => {
            {job_title}
           </h1>
   
-          <p className='mt-2 text-sm text-gray-600 '>
-            Lorem ipsum dolor sit adipisicing elit...
+          <p title={description} className='mt-2 text-sm text-gray-600 '>
+           {description.substring(0, 70)}...
           </p>
           <p className='mt-2 text-sm font-bold text-gray-600 '>
-            Range: $100 - $150
+            Range: ${min_price} - ${max_price}
           </p>
         </div>
       </Link>
