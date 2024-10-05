1486b2741db9e666f5055ebc89131f4dc3bcdec7
yeasinarafat51 <aloondokar65@gmail.com>
2024-10-05 13:31:07 +0600

    rej

--- src/pages/BidRequests.jsx
+++ src/pages/BidRequests.jsx
@@ -186,9 +186,9 @@ const BidRequests = () => {
                           </button>
                           {/* Reject Button */}
                           <button
-                            // onClick={() =>
-                            //   handleStatus(bid._id, bid.status, 'Rejected')
-                            // }
+                            onClick={() =>
+                              handleStatus(bid._id, bid.status, 'Rejected')
+                            }
                             disabled={bid.status === 'Complete'}
                             className='disabled:cursor-not-allowed text-gray-500 transition-colors duration-200   hover:text-yellow-500 focus:outline-none'
                           >
