c9039c29acadecab5a9a02db973bac1ac4ba5680
yeasinarafat51 <aloondokar65@gmail.com>
2024-10-07 18:06:48 +0600

    rtk

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -33,3 +33,5 @@ const useAxiosSecure = () => {
 }
 
 export default useAxiosSecure
+
+// mypostedjob
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,16 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes.jsx'
 import AuthProvider from './provider/AuthProvider.jsx'
 import { Toaster } from 'react-hot-toast'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
+   
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={router}/>
+    </QueryClientProvider >
     <Toaster/>
     </AuthProvider>
   </StrictMode>,
