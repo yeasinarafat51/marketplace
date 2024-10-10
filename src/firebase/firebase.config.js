72496682f9841570014e67ad734330809244ae65
yeasinarafat51 <aloondokar65@gmail.com>
2024-10-10 14:21:48 +0600

    newpro

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
+// TODO: Add SDKs for Firebase products that you want to uses
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
